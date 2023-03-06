c3339da6a633205a7562b9d9040948e85eedd993
SrPDev25 <ortegachkn25@gmail.com>
2023-03-06 08:32:32 +0100

    empezando con NODE

---
+++ YT_Desarrollo_Util/global_objects/global.js
@@ -0,0 +1,29 @@
+//Datos del sistema
+/**
+ * Window,
+ * es un objeto original o raiz en donde se encuentran el resto de propiedades
+ * como console, document, location...
+ * node no tiene window ya que esta es una propiedad de los navegadores
+ */
+//global.console.log(windows);
+/**
+ * Global,
+ * al igual que window en los navegadores 'global' es un objeto principal
+ * donde se encuentran muchas pripiedades y funciones, 
+ * si al crear una variable no la creamos con 
+ */
+//global.console.log(global);//Los datos del sistema igual que en un navegador es window
+
+
+
+
+//Un timeout, ejecuta un codigo después de un tiempo determinado
+/*setTimeout(() => {
+    console.log("Han pasado 2 segundos");
+    console.log("queso");
+}, 2000);
+setTimeout(() => 
+    console.log("Este va después en código pero antes en tiempo")
+, 1000);*/
+
+
