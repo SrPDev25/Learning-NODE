6d03745738fa2251097616bb9a50a879ef1a8c13
SrPDev25 <ortegachkn25@gmail.com>
2023-03-07 10:18:56 +0100

    I said FS

--- YT_Fazt/module/index.js
+++ YT_Fazt/module/index.js
@@ -8,14 +8,19 @@ let user = {
   lastname: "jhon",
 }; */
 
-
-//Forma antigua de require
+//Saludos
+/* //Forma antigua de require
 const persona=require('./myModule')
 //Nueva forma de require, have to use the same name
 const {saludo,despedida}=require('./myModule')
 
 //Cuando solo se importa un dato, el objeto representa a ese dato
 //Cuando es un conjunto de variables se puede elegir una de ellas
-console.log(saludo)
+console.log(saludo) */
 
 
+//Mates, importado uno de mis modulos
+const math=require('../math/index')
+
+
+console.log(math.add(1,2))
---
+++ YT_Fazt/module/os.js
@@ -0,0 +1,4 @@
+//os, importado un modulo de NODEjs
+const os=require('os')//os, datos dle sistema
+
+console.log(os.userInfo())
