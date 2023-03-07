48caab811e2719483936cf7cd5f9c9af16016511
SrPDev25 <ortegachkn25@gmail.com>
2023-03-07 09:45:13 +0100

    Nuevo canal

---
+++ YT_Fazt/base/index.js
@@ -0,0 +1,21 @@
+/* let username = "fazt";
+let age = 30;
+let hasHobbies = falselet;
+let points = [10, 20, 30];
+//Se puede annadir un .json como variable
+let user = {
+  name: "ryan",
+  lastname: "jhon",
+}; */
+
+
+//Forma antigua de require
+const persona=require('./myModule')
+//Nueva forma de require, have to use the same name
+const {saludo,despedida}=require('./myModule')
+
+//Cuando solo se importa un dato, el objeto representa a ese dato
+//Cuando es un conjunto de variables se puede elegir una de ellas
+console.log(saludo)
+
+
---
+++ YT_Fazt/base/myModule.js
@@ -0,0 +1,13 @@
+const saludo ='bonjour'
+const despedida='ourboir'
+
+/* const group={
+    saludo,
+    despedida
+}
+module.exports=group */
+
+module.exports.saludo=saludo
+module.exports.despedida=despedida
+
+//console.log(module)
