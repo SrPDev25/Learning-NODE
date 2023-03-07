171c415664df246ff3872a8cd72889c06a529308
SrPDev25 <ortegachkn25@gmail.com>
2023-03-07 10:58:01 +0100

    Dandole duro

---
+++ YT_Fazt/module/fs.js
@@ -0,0 +1,19 @@
+const fs=require('fs')
+
+//Codigo sincrono
+/* //just the path, you have the buffer
+const firstBuffer=fs.readFileSync('../data/first.txt')
+//with 'utf-8', which refered the system characters system, can had the text
+const firstReadable=fs.readFileSync('../data/first.txt','utf-8')
+
+console.log(firstReadable)      
+
+//If we write a file and didn't exist, we add it
+//this line overwrite
+fs.writeFileSync('../data/third.txt', 'this is a new file')
+
+const text= "text "
+
+fs.writeFileSync('../data/fourth.txt',text,{
+    flag:'a' //tag that means, not overwrite
+}) */
