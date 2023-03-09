fd7348aa1b1376648c196bd951247f305f1e6b34
Daniel Ortega <ortegachkn25@gmail.com>
2023-03-09 12:47:26 +0100

    fin del primer proyecto

--- YT_Fazt/REACT/first/src/index.js
+++ YT_Fazt/REACT/first/src/index.js
@@ -1,13 +1,14 @@
 //Se importa la libreria de react
-import React from "react";
+import React,{useState, useEffect} from "react";
 //Se importa la interfaz del usuario de React
 import ReactDOM from "react-dom/client";
 import { Button } from "./Button";
 
 import { Greeting, UserCard } from "./greetings";
 import { TaskCard } from "./Task";
-import {Post} from "./Post"
-import x from './users.json'
+import { Post } from "./Post";
+import x from "./users.json";
+
 
 // In React, must be declared 'root', because
 // is the index.html file for the library
@@ -15,11 +16,40 @@ import x from './users.json'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //Save the input's setOnChange
-const handerChange= (event)=>{
-  console.log(event.target.value)
+const handerChange = (event) => {
+  console.log(event.target.value);
+};
+
+const users = [x.User1, x.User2, x.User3];
+//  Hooks
+const Counter=()=>{
+  const [count,setCounter]= useState(0)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCounter(count+1)
+    },1000)
+  },[count])
+
+  return <div><h1>Counter: {count}</h1>
+    <button onClick={()=>{
+      setCounter(count+1)
+    }}>
+      Sumar
+    </button>
+    <button onClick={()=>{
+      setCounter(count-1)
+    }}>
+      Restar
+    </button>
+    <button onClick={()=>{
+      setCounter(count-1)
+    }}>
+      Input
+    </button>
+    </div>
 }
 
-const users=[x.User1,x.User2,x.User3]
+//    Render
 
 root.render(
   //Every almost every elements start with divs
@@ -33,26 +63,35 @@ root.render(
     />
     <TaskCard ready={true} />
     <UserCard user="juam" />
-    <Button text="Click here"/>
-    <Button text="Gtoo "/>
+    <Button text="Click here" />
+    <Button text="Gtoo " />
     {}
-    <input onChange={handerChange}/>
-    <form onSubmit={(e)=>{
-      e.preventDefault()
-      console.log('enviado')
-    }}>
+    <input onChange={handerChange} />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log("enviado");
+      }}
+    >
       <h1>form</h1>
-      <Button text='Enviar'>Hola</Button>
+      <Button text="Enviar">Hola</Button>
     </form>
-    <Post/>
-    {
-      users.map((user,index)=>{
-        //React like the keys code
-        //return only found with one element (use div if you'll use more than one)
-        return <div key={index}><h1 >{user.id}, {user.name}</h1>
-          <img src={user.image}/>
+    <Post />
+    {/* Hooks */}
+    <Counter/>
+
+    {/* maps */}
+    {users.map((user, index) => {
+      //React like the keys code
+      //return only found with one element (use div if you'll use more than one)
+      return (
+        <div key={index}>
+          <h1>
+            {user.id}, {user.name}
+          </h1>
+          <img src={user.image} />
         </div>
-      })
-    }
+      );
+    })}
   </>
 );
