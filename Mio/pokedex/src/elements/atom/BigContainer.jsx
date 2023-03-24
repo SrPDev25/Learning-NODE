7e0c9b90554d6140a8d208f7ae569e7daa0685fe
Daniel Ortega <ortegachkn25@gmail.com>
2023-03-24 12:12:55 +0100

    Sign done, and created headStyles

---
+++ Mio/pokedex/src/elements/atom/BigContainer.jsx
@@ -0,0 +1,7 @@
+export const BigContainer = (props) => {
+  return (
+    <div className="sign">
+      <p>{props.text}</p>
+    </div>
+  );
+};
--- Mio/pokedex/src/elements/organism/Head.jsx
+++ Mio/pokedex/src/elements/organism/Head.jsx
@@ -1,21 +1,24 @@
 import { Logo } from "../atom/Logo";
-import {  Start } from "../tools/reducerControl";
+import { Start } from "../tools/reducerControl";
 import { Box } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-
+import { BigContainer } from "../atom/BigContainer";
 
 export const Head = () => {
   //Another form for dispatch the store
-  const distpatch=useDispatch()
+  const distpatch = useDispatch();
   //Function which the web execute
   const click = () => {
     distpatch(Start());
   };
+  const initialized=useSelector((state)=>state.initialize)
+  
 
   return (
     <div className="head">
-      <Box display="flex" flexDirection="row" gap={1}>
-        <Logo width="80" action={click} />
+      <Box display="flex" gap={1}>
+        <Logo width="80" action={click} style={{ flex: "8" }} />
+        {initialized===0 && <BigContainer text="&#8592; Click aquí para empezar"/>}
       </Box>
     </div>
   );
