ccdafd367ef6bfb1294cec6f83245db6b095e065
Daniel Ortega <ortegachkn25@gmail.com>
2023-11-11 11:18:36 +0100

    tfc-learn, comentarios sobre lo que se hizo en el index

--- YT_Midu/express/src/index.ts
+++ YT_Midu/express/src/index.ts
@@ -1,4 +1,8 @@
+//Index sinedo la principal direccion de la api
 import express from 'express'
+
+//Se crea instacia de experss
+//entiendo instancia como el objeto que tiene las funciones nativas de la api
 const app = express()
 //Middleware
 /*Los middleware son de de alguna manera los conectores
@@ -12,13 +16,17 @@ const app = express()
  */
 app.use(express.json()) 
 
+//El puerto del servidor al que va a estar escuchando la api
+//TODO poner el puerto en variable de entorno
 const PORT = 3000
 
+//Esto ya se puede determinar como un endpoint(get)
 app.get('/ping', (_req, res) => {
     console.log('Perdi el tren :(')
     res.send('pero que miras, bobo')
 })
 
+//Inicia la escucha por parte de la instacia de express en un puerto concreto
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
 })
